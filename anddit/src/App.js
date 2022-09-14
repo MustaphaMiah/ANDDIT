3117980801eea0b851202f0dc3e460cfbe03184a
Mustapha Miah <mustaphamiah@L69PHH7RFP.local>
2022-09-14 12:34:10 +0100

    layout setup

--- anddit/src/App.js
+++ anddit/src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Profile from './Components/profiles';
+import logo from '../src/Assets/Images/andlogo.png'
+import axios from 'axios'
+
 
 function App() {
+
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(() => {
+    axios('http://localhost:3000').then(({ data }) => {
+      setProfiles(data)
+    }).catch((error) => console.log(error))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Anndit</h1>
+        <img src={logo} alt="Logo" />
       </header>
+      <div><h2>And Profile</h2>
+        <Profile />
+      </div>
     </div>
   );
 }
